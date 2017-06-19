bf33a9dd57a217941c08b8541f00cf94634d40ef
Nagarajnaidu921 <nagarajnaidu921@gmail.com>
2017-06-19 21:20:59 +0530

    pattern is added to mongoose schema to validate regNum and mogoose-validators is used

--- app/model/signup.js
+++ app/model/signup.js
@@ -1,9 +1,10 @@
 'use strict';
 const mongoose = require('./db.js');
+validators = require('mongoose-validators');
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	name: String,
-	regNum: String,
+	regNum: {type: String, validate: validators.matches('/[1-9][1-9](au|ae|ag|bm|ce|cp|cs|ee|ei|ec|it|mc|me|ma)[0-9][0-9][0-9]/gm') },
 	email: String,
 	password: String,
 	token: String
