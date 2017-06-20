cb8bf18982025ad74ed5959440b8b5d86804bdb4
Nagarajnaidu921 <nagarajnaidu921@gmail.com>
2017-06-20 06:51:35 +0530

    custome pattern is used in both mongodb schema and angular js to validate register number of the student

--- app/model/signup.js
+++ app/model/signup.js
@@ -3,7 +3,7 @@ const mongoose = require('./db.js');
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	name: {type: String, required: true},
-	regNum: {type: String, required: true},
+	regNum: {type: String, required: true, validate: /[1-9][1-9](au|ae|ag|bm|ce|cp|cs|ee|ei|ec|it|mc|me|ma)[0-9][0-9][0-9]/ },
 	email: {type: String, required: true},
 	password: {type: String, required: true},
 	token: {type: String, required: true},
